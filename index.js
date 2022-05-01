98896410562b4e624ae2df5d7d44fb989ec7b090
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-30 23:14:45 -0400

    commit tree 6162a9f0471fa6166d828bfcf60f98989f2f429c

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Mary',
+    streetAddress: '325 Central Avenue'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updateEmployee = {...employee};
+    updateEmployee[key] = value;
+    return updateEmployee;
+} // non-destructively updates employee with key and value //
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+} // destructively updates employee with key and value //
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    delete newEmployee.name;
+    return newEmployee;
+} // non-destructively deletes key from employee
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee;
+
+}
+// destructively deletes key from employee
+
+
+
+
